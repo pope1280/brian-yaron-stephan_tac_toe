8acab87e2448478815ad5b3374710d931ae4eac6
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-31 14:45:04 -0700

    Added check turn logic. Field check on the model (app/models/match.rb), content returned on the route (app/controllers/game.rb) and js setInterval function(public/js/turn.js)

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,24 @@
 class Matches < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
+
+  def board_pull
+    self.board
+  end
+
+  def turn(user_id)
+    if self.user_id == user_id
+      return check_turn("1")
+    else
+      return check_turn("2")
+    end
+  end
+
+  def check_turn(id)
+    if self.turn == id
+      {turn: "go"}
+    else
+      {turn: "wait"}
+    end
+  end
 end
---
+++ public/js/turn.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  var game_id = location.href.split("game/").pop();
+  var turn = null;
+
+  function checkTurn(game_id) {
+    $.ajax({
+      type: 'get',
+      url: ('/game' + game_id + '/turn')
+    }).done(function(data){
+      if (data.turn === "go") {
+        console.log("you can play now");
+      }
+    });
+  }
+
+  window.setInterval(function() {
+    checkTurn(game_id);
+  }, 5000);
+});
