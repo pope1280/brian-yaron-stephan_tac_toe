a030e75e862fbf2eccb80e195d9c24b3660ef20c
DBC-Apprentice <dbc@dbc.com>
2013-05-31 15:32:26 -0700

    board update function

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,39 @@
 Game ={
   begin: function(){
     $('CELL SELECTED').on('click', function(){
-      adMove(CELL SELECTED, X OR O(WHICH USER LOGGED IN) );
+      addMove(CELL SELECTED, X OR O(WHICH USER LOGGED IN) );
     });
   };
 
   function addMove(cell, xOrO){
-    ITEM TO BE CHOSEN.css("background-image", '(url)"../images/'+ xOrO +'.png"');
+    cell.css("background-image", '(url)"../images/'+ xOrO +'.png"');
   };
+
+  function updateBoard(string){
+    new_board = string.split('');
+
+  }
+
+  var game_id = location.href.split("game/").pop();
+  var turn = null;
+
+  function checkTurn(game_id) {
+    $.ajax({
+      type: 'get',
+      url: ('/game' + game_id + '/turn')
+    }).done(function(data){
+      if (data.turn === "go") {
+        updateBoard(data.board);
+        console.log("you can play now");
+      }
+    });
+  }
 }
 
 $(document).ready(function() {
   Game.begin();
+
+  window.setInterval(function() {
+    checkTurn(game_id);
+  }, 5000);
 });
